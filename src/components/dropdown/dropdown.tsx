fa6d449e0d4923fb216d63c43917720bf7424c02
Vamba45 <vambafool@bk.ru>
2024-01-25 15:33:26 +0300

    add dropdown arrow animation

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -7,10 +7,24 @@ interface IDropDown {
 }
 
 const DropDown: FC<IDropDown> = ({menuItems}) => {
+
+    function dropdownArrowClick(event: React.MouseEvent<HTMLButtonElement>) {
+        const btn = (event.target as HTMLElement); 
+        btn.style.pointerEvents = "none";
+
+        btn.classList.toggle('rotate')
+
+        setTimeout(() => {
+            btn.style.pointerEvents = "auto";
+        }, 300)
+    }
+
     return (
         <div className="dropdown">
             <div className="dropdown__select">
-                text
+                <div className="dropdown__text"></div>
+                <button className="dropdown__arrow"
+                        onClick={dropdownArrowClick}></button>
             </div>
             <ul className="dropdown__menu">
                 {
