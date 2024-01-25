3f0d9cb546d419dc27f89d0e09156a92941471c3
Vamba45 <vambafool@bk.ru>
2024-01-25 11:51:23 +0300

    add masked textinput

--- src/components/textField/textField.tsx
+++ src/components/textField/textField.tsx
@@ -1,15 +1,24 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './textField.scss';
+import { useMask } from '@react-input/mask';
 
 interface ITextField {
     placeholder?: string,
-    mask?: string,
+    inputmask?: string,
     isSubscribtion?: boolean;
 }
 
-const TextField: FC<ITextField> = ({placeholder, isSubscribtion = false}) => {
+const TextField: FC<ITextField> = ({placeholder, isSubscribtion = false, inputmask=""}) => {
+    const maxWidth = isSubscribtion ? "267px" : "320px";
+
+    const inputRef = useMask({ mask: inputmask, 
+                            replacement: { _: /\d/  },
+                            showMask: false,
+                        });
+
     return (
-        <div className="textField">
+        <div className="textField"
+            style={{maxWidth: maxWidth}}>
             { 
                 isSubscribtion === false &&
                 <input type="text" 
@@ -24,6 +33,13 @@ const TextField: FC<ITextField> = ({placeholder, isSubscribtion = false}) => {
                         placeholder={placeholder}/> 
                     <button className='textField__button'></button>
                 </>
+            }
+            {
+                inputmask !== "" && 
+                <input type="text" 
+                    className='textField__masked' 
+                    placeholder="ДД.ММ.ГГГГ"
+                    ref={inputRef}/>
             }
         </div>
     )
--- src/pages/UI Kit/formElements/formElements.tsx
+++ src/pages/UI Kit/formElements/formElements.tsx
@@ -1,10 +1,11 @@
 import { TextField } from '../../../components/textField/textField';
-import './formElements.scss';
+import './formelements.scss';
 
 const FormElements = () => {
     return (
         <div className="formElements uikit">
             <TextField placeholder='Email' isSubscribtion={true}/>
+            <TextField placeholder='Email' inputmask='__.__.____'/>
             <TextField placeholder='Email'/>
         </div>
     )
