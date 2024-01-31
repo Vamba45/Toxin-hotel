567cc26503cd2a5befccf5f51466f880b4dc571d
Vamba45 <vambafool@bk.ru>
2024-01-31 19:30:20 +0300

    add variations to  checkbox component

--- src/components/checkboxx/checkbox.tsx
+++ src/components/checkboxx/checkbox.tsx
@@ -4,15 +4,36 @@ import './checkbox.scss';
 interface ICheckBox {
     name: string;
     id: string;
-    labeltext: string;
+    title?: string;
+    paragraph: string;
     disabled?: boolean;
 }
 
-const Checkbox: FC<ICheckBox> = ({name, id, labeltext, disabled=false}) => {
+const Checkbox: FC<ICheckBox> = ({name, id, title=undefined, paragraph, disabled=false}) => {
     return (
         <div className="checkbox">
-            <input className="checkbox__input" type="checkbox" disabled={disabled} name={name} id={id}/>
-            <label className="checkbox__label" htmlFor={id}>{labeltext}</label>
+            <input className="checkbox__input" type="checkbox" disabled={disabled} name={name} id={name+id}/>
+            <label className="checkbox__label" htmlFor={name+id}>
+                <div>
+                    {
+                        title !== undefined && 
+                        <>
+                            <div className="checkbox__title">
+                                {
+                                    title
+                                }
+                            </div>
+                            <div className="checkbox__paragraph">
+                                {paragraph}
+                            </div>
+                        </>
+                    }
+                    {
+                        title === undefined && 
+                        paragraph
+                    }
+                </div>
+            </label>
         </div>
     )
 }
