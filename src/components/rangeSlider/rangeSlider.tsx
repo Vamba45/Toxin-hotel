2cdb52e20b68e342417f787d0f2c84785e576107
Vamba45 <vambafool@bk.ru>
2024-02-02 16:43:59 +0300

    add functional to rangeslider

--- src/components/rangeSlider/rangeSlider.tsx
+++ src/components/rangeSlider/rangeSlider.tsx
@@ -1,30 +1,81 @@
 import { FC } from 'react';
 import './rangeSlider.scss';
 
-const RangeSlider: FC = () => {
+interface IRangeSlider {
+    priceGap: number;
+    maxValue: number;
+    defaultMin: number;
+    defaultMax: number;
+    title: string;
+}
+
+const RangeSlider: FC<IRangeSlider> = ({priceGap, defaultMax, defaultMin, title, maxValue}) => {
+
+    function rangeOnInput(e: React.FormEvent<HTMLInputElement>) {
+        const target = e.target as HTMLInputElement;
+        const parent = target.closest('.rangeSlider')
+
+        const rangeMin = parent?.querySelector('.range-min') as HTMLInputElement;
+        const rangeMax = parent?.querySelector('.range-max') as HTMLInputElement;
+        const inputMin = parent?.querySelector('.input-min') as HTMLInputElement;
+        const inputMax = parent?.querySelector('.input-max') as HTMLInputElement;
+        const progress = parent?.querySelector('.rangeSlider__progress') as HTMLElement;
+
+        let minVal = Number(rangeMin.value),
+            maxVal = Number(rangeMax.value);
+
+        if(maxVal - minVal < priceGap) {
+            if(target.className === "range-min") {
+                rangeMin.value = `${maxVal - priceGap}`;
+            } else {
+                rangeMax.value = `${minVal + priceGap}`;
+            }
+        } else {
+            inputMin.textContent = String(minVal) + "₽";
+            inputMax.textContent = String(maxVal) + "₽";
+
+            progress.style.left = (Number(rangeMin.value) / Number(rangeMin.max)) * 100 + "%";
+            progress.style.right = 100 - (Number(rangeMax.value) / Number(rangeMax.max)) * 100 + "%";
+        }
+    }
+
+    let left = (defaultMin / maxValue) * 100 + "%";
+    let right = (maxValue - defaultMax) / maxValue * 100 + "%";
+
     return (
         <div className="rangeSlider">
-            <div className='price-input'>
-                <div className="field">
-                    <span>Min</span>
-                    <input type="number" className='input-min' defaultValue="2500"/>
-                </div>
-                <div className="separator">
-                    -
-                </div>
-                <div className="field">
-                    <span>Max</span>
-                    <input type="number" className='input-max' defaultValue="7500"/>
+            <div className="rangeSlider__output">
+                <div className="rangeSlider__container">
+                    <div className="rangeSlider__title">
+                        {title}
+                    </div>
+                    <div className="rangeSlider__inputs">
+                        <div className='input-min'>
+                            {defaultMin}₽
+                        </div>
+                        <span className='rangeSlider__separators'>-</span>
+                        <div className='input-max'>
+                            {defaultMin}₽
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="slider">
-                <div className="progress">
-
+            <div className="rangeSlider__slider">
+                <div className="rangeSlider__progress"
+                    style={{
+                        left: left,
+                        right: right
+                    }}>
                 </div>
             </div>
             <div className="range-input">
-                <input type="range" className="range-min" min="0" max="10000" defaultValue="2500"/>
-                <input type="range" className="range-max" min="0" max="10000" defaultValue="2500"/>
+                <input type="range" className="range-min" 
+                min="0" max={maxValue} defaultValue={defaultMin}
+                onInput={rangeOnInput}
+                step={100}/>
+                <input type="range" className="range-max" min="0" max={maxValue} defaultValue={defaultMax}
+                onInput={rangeOnInput}
+                step={100}/>
             </div>
         </div>
     )
--- src/pages/UI Kit/formElements/formElements.tsx
+++ src/pages/UI Kit/formElements/formElements.tsx
@@ -72,8 +72,10 @@ const FormElements = () => {
 
             <Comment avatar={avatar} likes={12} lastvisit='5 дней назад'
                         username='Мурад Сарафанов' text='Великолепный матрас на кровати в основной спальне! А пуфик вообще потрясающий. И стены, действительно, шумоподавляющие. Выкрикивал комплименты повару — никто не жаловался из соседей.'/>
-            <RangeSlider/>
-        </div>
+            
+            <RangeSlider maxValue={15000} defaultMax={10000} defaultMin={5000} priceGap={1500} title='range slider'/>
+            <RangeSlider maxValue={10000} defaultMax={7500} defaultMin={2500} priceGap={1000} title='range slider'/>
+        </div> 
     )
 }
 
