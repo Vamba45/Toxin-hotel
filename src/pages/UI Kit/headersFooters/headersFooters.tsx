27afb3c34d2606c4556885ee52bd9d47e6371135
Vamba45 <vambafool@bk.ru>
2024-02-15 07:20:54 +0300

    add header adaptive for user-variant

--- src/pages/UI Kit/headersFooters/headersFooters.tsx
+++ src/pages/UI Kit/headersFooters/headersFooters.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import './headersFooters.scss';
 import Header from "../../../components/header/header";
+import Footer from "../../../components/footer/footer";
 
 const HeadersFooters: FC = () => {
     return (
         <div className="headfoot">
             <Header/>
             <Header user="Юлий Цезарь"/>
+
+            <Footer/>
         </div>
     )
 }
