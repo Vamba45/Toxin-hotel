d201823080fca364b6cad7d0330886e276e4461d
Vamba45 <vambafool@bk.ru>
2024-02-20 02:07:27 +0300

    add room component

---
+++ src/components/room/room.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react';
+import './room.scss';
+
+import Slider from '../slider/slider';
+import Rate from '../rate/rate';
+
+interface IRoom {
+    isLuxe?: boolean,
+    price: number,
+    reviews: number,
+    starsCount?: number,
+    activeStars?: number,
+    starsName: string,
+    number: number,
+    sliderItems: any[]
+}
+
+const Room : FC<IRoom> = ({isLuxe = false, price, reviews, starsName, starsCount = 5, number, sliderItems, activeStars = 0}) => {
+    return (
+        <div className="room">
+            <div className="room__slider">
+                <Slider items={sliderItems}/>
+            </div>
+            <div className="room__info info">
+                <div className="info__rows">
+                    <div className="info__column">
+                        <div className="info__number">
+                            <span className='num'>№</span>
+                            <span className='numeric'>{ number } </span>
+                            {
+                                isLuxe && <span className='luxe'>люкс</span>
+                            }
+                        </div>
+                        <div className="info__price">
+                            <span className='numeric'>{ price }₽</span> в сутки 
+                        </div>
+                    </div>
+                    <div className="info__column">
+                        <div className="info__rate">
+                            <Rate name={starsName} starCount={5} activeStars={activeStars}/>
+                        </div>
+                        <div className="info__reviews">
+                            <span className='numeric'>{ reviews }</span> Отзывов
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Room;
--- src/pages/UI Kit/cards/cards.tsx
+++ src/pages/UI Kit/cards/cards.tsx
@@ -7,11 +7,13 @@ import card3 from '../../../assets/img/cards/3.png';
 import card4 from '../../../assets/img/cards/4.png';
 
 import Slider from '../../../components/slider/slider';
+import Room from '../../../components/room/room';
 
 const Cards: FC = () => {
     return(
         <div className='cards uikit'>
-            <Slider items={[card1, card2, card3, card4]}/>
+            <Room isLuxe={true} number={888} price={9900} reviews={145} starsName='1room' sliderItems={[card1, card2, card3, card4]} activeStars={5}/>
+            <Room isLuxe={false} number={840} price={9900} reviews={65} starsName='2room' sliderItems={[card1, card2, card3, card4]} activeStars={4}/>
         </div>
     )
 }
