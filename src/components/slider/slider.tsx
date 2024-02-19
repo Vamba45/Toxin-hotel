089020be9303401266ac725d06487afccad4dfff
Vamba45 <vambafool@bk.ru>
2024-02-19 10:09:53 +0300

    finish slider component

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -6,12 +6,76 @@ interface ISlider {
 }
 
 const Slider : FC<ISlider> = ({items}) => {
+
+    function dotClick(e: React.MouseEvent<HTMLElement>) {
+        const slider = (e.target as HTMLElement).closest('.slider');
+
+        const frame = slider?.querySelector('.slider__frames') as HTMLElement;
+        const dots = slider?.getElementsByClassName('dot') as HTMLCollection;
+
+        for(let i = 0; i < dots.length; i++) {
+
+            if(dots[i].classList.contains('active')) {
+                    dots[i].classList.remove('active')
+            }
+
+            if(dots[i] === e.target) {
+                dots[i].classList.add('active')
+                frame.style.transform = `translateX(-${Number(slider?.clientWidth)*i}px)`;
+            }
+        }
+    }
+
+    function nextClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const slider = (e.target as HTMLElement).closest('.slider');
+
+        const frame = slider?.querySelector('.slider__frames') as HTMLElement;
+        const dots = slider?.getElementsByClassName('dot') as HTMLCollection;
+
+        for(let i = 0; i < dots.length; i++) {
+
+            if(dots[i].classList.contains('active')) {
+
+                if(i !== dots.length - 1) {
+                    dots[i].classList.remove('active')
+                    dots[i + 1].classList.add('active')
+
+                    frame.style.transform = `translateX(-${Number(slider?.clientWidth)*(i+1)}px)`;
+
+                    break
+                }
+            }
+        }
+    }
+
+    function prevClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const slider = (e.target as HTMLElement).closest('.slider');
+
+        const frames = slider?.querySelector('.slider__frames') as HTMLElement;
+        const dots = slider?.getElementsByClassName('dot') as HTMLCollection;
+
+        for(let i = 0; i < dots.length; i++) {
+
+            if(dots[i].classList.contains('active')) {
+
+                if(i !== 0) {
+                    dots[i].classList.remove('active')
+                    dots[i - 1].classList.add('active')
+
+                    frames.style.transform = `translateX(-${Number(slider?.clientWidth)*(i - 1)}px)`;
+
+                    break
+                }
+            }
+        }
+    }
+
     return (
         <div className="slider">
             <ul className="slider__frames">
                 {
-                    items.map((item) => (
-                        <li className="frame">
+                    items.map((item, index) => (
+                        <li className={"frame" + " " + (index === 0 ? "active" : "")}>
                             <img src={item} alt="slider frame" />
                         </li>
                     ))
@@ -19,15 +83,18 @@ const Slider : FC<ISlider> = ({items}) => {
             </ul>
             <div className="slider__dots">
                 {
-                    items.map(() => (
-                        <div className="dot"></div>
+                    items.map((item, index) => (
+                        <div className={"dot" + " " + (index === 0 ? "active" : "")}
+                            onClick={dotClick}></div>
                     ))
                 }
             </div>
             <div className="slider__buttons">
-                <button className="slider__prev">
+                <button className="slider__prev"
+                    onClick={prevClick}>
                 </button>
-                <button className="slider__next">
+                <button className="slider__next"
+                    onClick={nextClick}>
                 </button>
             </div>
         </div>
