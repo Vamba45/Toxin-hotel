3affa470ac5cb79ce2b60db426893b6dfeba07b4
Vamba45 <vambafool@bk.ru>
2024-04-14 00:42:14 +0300

    add cleaning btn in date-range picker

--- src/components/dateRangePicker/dateRangePicker.tsx
+++ src/components/dateRangePicker/dateRangePicker.tsx
@@ -1,12 +1,54 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import './dateRangePicker.scss';
 import { DatePicker } from "@mui/x-date-pickers";
+import * as dayjs from 'dayjs';
 
 const DateRangePicker: FC = () => {
+    const [minDate, setMinDate] = useState<dayjs.Dayjs | null>();
+    const [maxDate, setMaxDate] = useState<dayjs.Dayjs | null>();
+    
+    const [cleared, setCleared] = useState<boolean>(false);
+
+    function ClearableProp() {
+        const [cleared, setCleared] = useState<boolean>(false);
+      
+        useEffect(() => {
+          if (cleared) {
+            const timeout = setTimeout(() => {
+              setCleared(false);
+            }, 1500);
+      
+            return () => clearTimeout(timeout);
+          }
+          return () => {};
+        }, [cleared]);
+    }
+
     return (
-        <>
-            <DatePicker/>
-        </>
+        <div className="daterange">
+            <DatePicker onAccept={(value) => {
+                if(maxDate?.isBefore(value)) {
+                    setMaxDate(value);
+                }
+
+                setMinDate(value);
+            }}
+            slotProps={{
+                field: { clearable: true, onClear: () => setCleared(true) },
+            }}
+            />
+
+            <div className="daterange__line">
+                -
+            </div>
+
+            <DatePicker minDate={minDate} value={maxDate} onAccept={(value) => {
+                setMaxDate(value);
+            }}
+            slotProps={{
+                field: { clearable: true, onClear: () => setCleared(true) },
+            }}/>
+        </div>
     )
 }
 
