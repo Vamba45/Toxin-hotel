9b0fb46a2a96ccc7265e4dcd18463ad4c652d0c3
Vamba45 <vambafool@bk.ru>
2024-04-19 00:03:08 +0300

    add content to landing page

--- src/pages/WebsitePages/Landing/Landing.tsx
+++ src/pages/WebsitePages/Landing/Landing.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 import './Landing.scss';
+import SearchRoom from "../../../components/searchRoom/searchRoom";
 
 const Landing: FC = () => {
     return (
         <div className="landing">
             <div className="container">
-
+                <div className="landing__rows">
+                    <div className="landing__column">
+                        <SearchRoom/>
+                    </div>
+                    <div className="landing__column">
+                        <p className="landing__text">
+                            Лучшие номера для вашей работы, <br />
+                            отдыха и просто вдохновения
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
