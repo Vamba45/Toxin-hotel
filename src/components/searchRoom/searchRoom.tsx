3f116dc683845d33b54531a83b4fec0cc1db5909
Vamba45 <vambafool@bk.ru>
2024-04-18 03:21:08 +0300

    add search-room comp

---
+++ src/components/searchRoom/searchRoom.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import './searchRoom.scss';
+
+import RangePicker from '../rangePicker/RangePicker';
+import DropDown from '../dropdown/dropdown';
+
+const SearchRoom : FC = () => {
+    return (
+        <form className="searchroom">
+            <div className="searchroom__rows">
+                <div className="searchroom__column">
+                    <h1 className='searchroom__title'>
+                        Найдём номера под <br />
+                        ваши желания
+                    </h1>
+                </div>
+                <div className="searchroom__column">
+                    <RangePicker/>
+                </div>
+                <div className="searchroom__column">
+                    <DropDown menuItems={["Взрослые", "Дети", "Младенцы"]}/>
+                </div>
+                <div className="searchroom__column">
+                    <button className="searchroom__btn">подобрать номер</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default SearchRoom;
