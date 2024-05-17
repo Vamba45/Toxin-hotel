d8e6a90cff52e5021fcb32eef8e51c9c61b93cba
Vamba45 <vambafool@bk.ru>
2024-05-17 23:39:53 +0300

    change pagination -component

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -42,77 +42,92 @@ const Pagination: FC<IPagination> = ({pageLimit = 15}) => {
         }
     }
 
+    if(pageLimit <= 0) {
+        return
+    }
+
     return(
         <div className="pagination">
             <ul className="pagination__list">
                 {
-                    page <= pageRange && 
-                            range(1, pageRange + 1).map((el) => (
-                                <li className={(page === el ? "active" : "") + " pag-btn"}
+                    pageLimit < 7 && <> {
+                        (Array.from(Array(pageLimit).keys())).map((num, i, array) => (<li className={(page === num + 1 ? "active" : "") + " pag-btn"} onClick={() => setPage(num + 1)}>
+                                {num + 1}
+                            </li>
+                        ))
+                    }    
+                    </>
+                }
+                {
+                    pageLimit >= 7 && <>
+                        {
+                            page <= pageRange && 
+                                    range(1, pageRange + 1).map((el) => (<li className={(page === el ? "active" : "") + " pag-btn"}
+                                            onClick={() => { 
+                                                setPage(el)
+                                            }}>{el}</li>))
+                        }
+
+                        {
+                            page > pageRange && 
+                            <>
+                                <li className="pagination__prev pag-btn"
+                                    onClick={() => {
+                                            pageDecriment();
+                                        }}></li>
+                                <li className="pagination__firstpage pag-btn"
+                                    onClick={() => { setPage(1); }}>
+                                    1
+                                </li>
+                                <li className="pagination__dotsbefore pag-btn"
                                     onClick={() => { 
-                                        setPage(el)
-                                    }}>{el}</li>
-                            ))
-                }
+                                        pageDecriment();
+                                    }}>
+                                    ...
+                                </li>   
+                            </>
+                        }
 
-                {
-                    page > pageRange && 
-                    <>
-                        <li className="pagination__prev pag-btn"
-                            onClick={() => {
-                                    pageDecriment();
-                                }}></li>
-                        <li className="pagination__firstpage pag-btn"
-                            onClick={() => { setPage(1); }}>
-                            1
-                        </li>
-                        <li className="pagination__dotsbefore pag-btn"
-                            onClick={() => { 
-                                pageDecriment();
-                            }}>
-                            ...
-                        </li>   
-                    </>
-                }
-
-                <div className="pagination__centerNums">
-                    {
-                        page > pageRange && page <= (pageLimit - pageRange) &&
-                        <>
+                        <div className="pagination__centerNums">
                             {
-                                centerNums.map((num, index) => (
-                                    <li className={(page === num ? "active" : "") + " pag-btn"}
-                                        onClick={() => setPage(num)}>
-                                        {num}
-                                    </li>
-                                ))
+                                page > pageRange && page <= (pageLimit - pageRange) &&
+                                <>
+                                    {
+                                        centerNums.map((num, index) => (
+                                            <li className={(page === num ? "active" : "") + " pag-btn"}
+                                                onClick={() => setPage(num)}>
+                                                {num}
+                                            </li>
+                                        ))
+                                    }
+                                </>
                             }
-                        </>
-                    }
-                </div>
+                        </div>
 
-                {
-                    page < (pageLimit - pageRange + 1) && 
-                    <>
-                        <li className="pagination__dotsafter pag-btn"
-                            onClick={pageIncrement}>
-                            ...
-                        </li>
-                        <li className="pagination__lastpage pag-btn"
-                            onClick={() => setPage(pageLimit)}>
-                            {pageLimit}
-                        </li>
-                        <li className="pagination__next pag-btn"
-                            onClick={pageIncrement}></li>
+                        {
+                            page < (pageLimit - pageRange + 1) && 
+                            <>
+                                <li className="pagination__dotsafter pag-btn"
+                                    onClick={pageIncrement}>
+                                    ...
+                                </li>
+                                <li className="pagination__lastpage pag-btn"
+                                    onClick={() => setPage(pageLimit)}>
+                                    {pageLimit}
+                                </li>
+                                <li className="pagination__next pag-btn"
+                                    onClick={pageIncrement}></li>
+                            </>
+                        }
+
+                        {
+                            page > (pageLimit - pageRange) && 
+                            range(pageLimit - pageRange, pageLimit).map((el) => (
+                                <li className={(page === el ? "active" : "") + " pag-btn"}
+                                    onClick={() => setPage(el)}>{el}</li>
+                            ))
+                        }
                     </>
-                }
-
-                {
-                    page > (pageLimit - pageRange) && 
-                    range(pageLimit - pageRange, pageLimit).map((el) => (
-                        <li className={(page === el ? "active" : "") + " pag-btn"}
-                            onClick={() => setPage(el)}>{el}</li>
-                    ))
                 }
             </ul>
             <div className="pagination__textbottom">1-12 из 100+ вариантов аренды</div>
