d7e9998314679c525320d6858641f90dd55ba0fc
Vamba45 <vambafool@bk.ru>
2024-06-05 10:37:26 +0300

    change daley useDebounce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react"
 
-export default function useDebounce(callback, delay: 500) {
+export default function useDebounce(callback, delay = 500) {
     const timer = useRef();
 
     const debouncedCallback = useCallback((...args) => {
