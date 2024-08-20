a582de76d634ad200f8da88213eb2edec1be1933
Vamba45 <vambafool@bk.ru>
2024-08-20 22:51:31 +0300

    change redux url

--- src/store/services/roomSerivce.ts
+++ src/store/services/roomSerivce.ts
@@ -4,7 +4,7 @@ import { ListResponse } from '../../model/IListResponce';
 
 export const roomsAPI = createApi({
     reducerPath: 'roomsAPI',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5050/api'}),
     endpoints: (build) => ({
         fetchAllRooms: build.query<ListResponse<IRoom>, string>({
             query: (filter) => ({
