17cdca9a15a62a0b72a50bdb99ff1b73e211163c
Vamba45 <vambafool@bk.ru>
2024-08-26 00:23:54 +0300

    add buttonClick param to SearchRoom

--- src/components/searchRoom/searchRoom.tsx
+++ src/components/searchRoom/searchRoom.tsx
@@ -3,8 +3,15 @@ import './searchRoom.scss';
 
 import RangePicker from '../rangePicker/RangePicker';
 import DropDown from '../dropdown/dropdown';
+import { useNavigate } from 'react-router';
 
-const SearchRoom : FC = () => {
+interface ISearchRoom {
+    buttonClick: (event: React.MouseEvent) => void;
+}
+
+const SearchRoom : FC<ISearchRoom> = ({buttonClick}) => {
+    const navigate = useNavigate();
+    
     return (
         <form className="searchroom">
             <div className="searchroom__rows">
@@ -21,7 +28,7 @@ const SearchRoom : FC = () => {
                     <DropDown menuItems={[{name: "Взрослые"}, {name: "Дети"}, {name: "Младенцы"}]} placeholder='Гости' commonName='Гостей'/>
                 </div>
                 <div className="searchroom__column">
-                    <button className="searchroom__btn">подобрать номер</button>
+                    <button className="searchroom__btn" onClick={buttonClick}>подобрать номер</button>
                 </div>
             </div>
         </form>
