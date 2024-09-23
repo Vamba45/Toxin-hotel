b27d7684021c8044cbd0910f1f2ab6c07578a10b
Vamba45 <vambafool@bk.ru>
2024-09-23 21:01:30 +0300

    add userService

---
+++ src/store/services/userService.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IUser } from '../../model/IUser';
+
+export const userAPI = createApi({
+    reducerPath: 'userAPI',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://toxin-backend-production.up.railway.app/'}),
+    endpoints: (build) => ({
+        fetchOneUser: build.query<IUser, string>({
+            query: (id) => ({
+                url: `/user`
+            })
+        }),
+    })
+})
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,18 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { roomsAPI } from "./services/roomSerivce";
 import userReducer from './slices/userSlice';
+import { userAPI } from "./services/userService";
 
 const rootReducer = combineReducers({
     userReducer,
-    [roomsAPI.reducerPath]: roomsAPI.reducer 
+    [roomsAPI.reducerPath]: roomsAPI.reducer,
+    [userAPI.reducerPath]: userAPI.reducer
 })
 
 export const setupStore = () => {
     return configureStore({
         reducer: rootReducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(roomsAPI.middleware)
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(roomsAPI.middleware, userAPI.middleware)
     })
 }
 
